a9082a266970024f3a059868c65af97ffdc8b38e
Guillaume Baur <guilaume.baur@gmail.com>
2021-07-29 12:12:56 +0200

    script.js génération entierAleatoire

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /*ROLLDICE */
-let ROUND = 0
+var ROUND = entierAleatoire(1, 6);
 
 const rollDice = document.getElementById('rollDice')
 const ROUND1 = document.getElementById('ROUND1')
 const ROUND2 = document.getElementById('ROUND2')
 
 const rolldice = () => {
-  ROUND++
+  var ROUND = entierAleatoire(1, 6);
   ROUND1.textContent = ROUND
   ROUND2.textContent = ROUND
 }
@@ -22,8 +22,17 @@ const GLOBAL2 = document.getElementById('GLOBAL2')
 
 const Hold = () => {
   GLOBAL++
-  GLOBAL1.textContent = ROUND
-  GLOBAL2.textContent = ROUND
+  GLOBAL1.textContent = ROUND1.textContent
+  GLOBAL2.textContent = ROUND2.textContent
 }
 
 hold.addEventListener('click', Hold)
+
+/*RANDOM*/
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//Utilisation
+//La variable contient un nombre aléatoire compris entre 1 et 6
+var entier = entierAleatoire(1, 6);
