1805b28ca3b23da1d576a3514d11227ba7a185c6
Guillaume Baur <guilaume.baur@gmail.com>
2021-07-29 11:30:35 +0200

    simplification des noms de classes

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let playerScore = 0
+/*ROLLDICE */
+let ROUND = 0
 
 const rollDice = document.getElementById('rollDice')
-const playerscore1 = document.getElementById('playerScore1')
-const playerscore2 = document.getElementById('playerScore2')
+const ROUND1 = document.getElementById('ROUND1')
+const ROUND2 = document.getElementById('ROUND2')
 
 const rolldice = () => {
-  playerScore++
-  playerscore1.textContent = playerScore
-  playerscore2.textContent = playerScore
+  ROUND++
+  ROUND1.textContent = ROUND
+  ROUND2.textContent = ROUND
 }
 
 rollDice.addEventListener('click', rolldice)
+
+/*HOLD*/
+let GLOBAL = 0
+
+const hold = document.getElementById('hold')
+const GLOBAL1 = document.getElementById('GLOBAL1')
+const GLOBAL2 = document.getElementById('GLOBAL2')
+
+const Hold = () => {
+  GLOBAL++
+  GLOBAL1.textContent = ROUND
+  GLOBAL2.textContent = ROUND
+}
+
+hold.addEventListener('click', Hold)
